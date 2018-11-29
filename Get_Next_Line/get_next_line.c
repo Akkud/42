7a8906ef2efa4ac15c197328b575317476d5c5ce
Paul CHARBONNELLE <pacharbo@e3r5p4.42.fr>
2018-11-29 21:55:15 +0100

    lblb

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -6,67 +6,63 @@
 /*   By: pacharbo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 14:38:19 by pacharbo          #+#    #+#             */
-/*   Updated: 2018/11/27 18:10:52 by pacharbo         ###   ########.fr       */
+/*   Updated: 2018/11/29 21:54:05 by pacharbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		check_list(char **str, size_t fd)
+int		check_list(char **str, t_list *lst, int fd)
 {
 	t_list	*alst;
 
 	alst = lst;
 	while (fd != alst->fd)
 	{
-		if (alst->(*next))
+		if (alst->next)
 			alst = alst->next;
 		else
-		{
-			if (!(alst->next = (t_list*)malloc(sizeof(t_list))))
-				return (-1);
-			alst = alst->next;
-			alst->fd = fd;
-		}
+			return (1);
 	}
-	if (alst->(*content))
+	if (alst->content)
 	{
 		if (!(*str = ft_strnew(ft_strlen(alst->content))))
 			return (-1);
 		ft_strcpy(*str, alst->content);
-		free(alst->content);
+		ft_strdel(&(alst->content));
 	}
 	return (1);
 }
 
-int		readqll(char **str)
+int		readqll(char **str, int fd)
 {
 	int 	lu;
 	char	tmp[BUFF_SIZE + 1];
 
-	a = 0;
 	lu = 1;
-	if (!**str)
-		if (!(*str = strnew(BUFF_SIZE)))
+	if (!*str)
+		if (!(*str = ft_strnew(BUFF_SIZE)))
 			return (-1);
 	while (!ft_strchr(*str, 10) && lu)
 	{
 		lu = read(fd, tmp, BUFF_SIZE);
 		tmp[BUFF_SIZE] = '\0';
-		if (!(*str = ft_strjoin(str, tmp)))
+		if (!(*str = ft_strjoin((const char*)*str, tmp)))
 			return (-1);
 	}
+	if (lu == 0)
+		return (0);
 	return (1);
 }
 
-int		last_ft(char **str)
+int		last_ft(char *tmp, t_list *lst, int fd)
 {
 	t_list	*alst;
 
 	alst = lst;
 	while (fd != alst->fd)
 	{
-		if (alst->(*next))
+		if (alst->next)
 			alst = alst->next;
 		else
 		{
@@ -74,31 +70,59 @@ int		last_ft(char **str)
 				return (-1);
 			alst = alst->next;
 			alst->fd = fd;
+			alst->next = NULL;
 		}
 	}
-	if (alst->(*content))
-	{
-		if (!(*str = ft_strnew(ft_strlen(alst->content))))
-			return (-1);
-		ft_strcpy(*str, alst->content);
-		free(alst->content);
-	}
+	if (!(alst->content = ft_strnew(ft_strlen(tmp))))
+		return (-1);
+	ft_strcpy(alst->content, tmp);
 	return (1);
 }
 
 int		get_next_line(const int fd, char **line)
 {
 	static t_list	*lst;
-	char		*str;
-	int		a;
+	char			*str;
+	char			*tmp;
+	int				lu;
 
 	if (!lst)
 		if (!(lst = (t_list*)malloc(sizeof(t_list))))
 			return (-1);
-	if (check_list(&str) < 0)
+	lst->next = NULL;
+	if (check_list(&str, lst, fd) < 0)
 		return (-1);
-	readqll(&str);
-	memccpy(line, str, 10, ft_strlen(str));
+	if ((lu = readqll(&str, fd)) < 0)
+		return (-1);
+	tmp = ft_memccpy(*line, str, 10, ft_strlen(str));
+	line[0][ft_strchr((const char*)*line, 10) - *line] = '\0';
+	if (!*tmp)
+		lu = lu - 23;
+	else
+		if (last_ft(tmp, lst, fd) < 0)
+			return (-1);
+	ft_strdel(&str);
+	if (lu == -23)
+		return (0);
+	return (1);
+}
 
+#include <stdio.h>
 
+int		main(int ac, char **av)
+{
+	int fd;
+	int ret;
+	char *line;
+
+	(void)ac;
+	line = ft_strnew(99999);
+	fd = open(av[1], O_RDONLY);
+	ret = 1;
+	while (ret > 0)
+	{
+		ret = get_next_line(fd,&line);
+		printf("%s\n", line);
+	}
+	return (0);
 }
--- Get_Next_Line/get_next_line.h
+++ Get_Next_Line/get_next_line.h
@@ -1,13 +1,13 @@
-/* ************************************************************************** */                                     
-/*                                                                            */                                     
-/*                                                        :::      ::::::::   */                                     
-/*   get_next_line.h                                    :+:      :+:    :+:   */                                     
-/*                                                    +:+ +:+         +:+     */                                     
-/*   By: pacharbo <marvin@42.fr>                    +#+  +:+       +#+        */                                     
-/*                                                +#+#+#+#+#+   +#+           */                                     
-/*   Created: 2018/08/29 18:39:23 by pacharbo          #+#    #+#             */                                     
-/*   Updated: 2018/08/29 21:37:27 by pacharbo         ###   ########.fr       */                                     
-/*                                                                            */                                     
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pacharbo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/29 19:26:20 by pacharbo          #+#    #+#             */
+/*   Updated: 2018/11/29 21:03:12 by pacharbo         ###   ########.fr       */
+/*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
@@ -20,9 +20,17 @@
 
 typedef struct		s_list
 {
-	char		*content;
-	int		fd;
+	char			*content;
+	int				fd;
 	struct s_list	*next;
 }		t_list;
+
+void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
+size_t			ft_strlen(const char *str);
+char			*ft_strcpy(char *dest, const char *src);
+char			*ft_strchr(const char *str, int c);
+void			ft_strdel(char **as);
+char			*ft_strnew(size_t size);
+char			*ft_strjoin(char const *s1, char const *s2);
 
 #endif
