9ec3d5be26276f192b6421e1a771f061e5c6737b
root <root@init.monpc>
2019-03-25 17:32:20 +0100

    taff sur printf

---
+++ ft_printf/main.c
@@ -0,0 +1,54 @@
+#include "ft_printf.h"
+
+static int	datalloc(t_printf **data)
+{
+	if (*data)
+	{
+		if (!(*(data)->next = (t_printf*)malloc(sizeof(t_printf))))
+			return (0);
+		*data = *(data)->next;
+	}
+	else
+		if (!(*data = (t_printf*)malloc(sizeof(t_printf))))
+			return (0);
+	data->flag = 0;
+	data->fw = NULL;
+	data->pres = NULL;
+	data->lmod = 0;
+	
+	
+	return (1);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list ap;
+	t_printf *data;
+
+	data = NULL;
+	va_start(ap, format);
+	while (*format)
+	{
+		if (*format++ == '%')
+		{
+			ft_datalloc(&data);
+			ft_cflag(&format, data);
+			ft_cfw(&format, data);
+			ft_cpres(&format, data);
+			ft_clmod(&format, data);
+			ft_cconv(&format, data);
+		}
+	}
+	ft_putstr(
+	va_end(ap);
+	return ();
+}
+
+int		main()
+{
+
+
+
+
+
+}
